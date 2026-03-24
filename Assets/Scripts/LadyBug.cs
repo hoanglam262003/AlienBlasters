9566ded52b5d40e1bcd0ac432bfa66c9acec1a2b
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2026-03-24 17:06:02 +0700

    ladybug turn around when have something block

--- Assets/Scripts/LadyBug.cs
+++ Assets/Scripts/LadyBug.cs
@@ -4,6 +4,8 @@ public class LadyBug : Enemy
 {
     [SerializeField]
     private float speed = 1f;
+    [SerializeField]
+    private float raycastDistance = 0.2f;
     private Vector2 direction = Vector2.left;
 
     private SpriteRenderer spriteRenderer;
@@ -18,6 +20,18 @@ public class LadyBug : Enemy
 
     private void Update()
     {
+        Vector2 offset = direction * col.bounds.extents.x;
+        Vector2 origin = (Vector2)transform.position + offset;
+        var hits = Physics2D.RaycastAll(origin, direction, raycastDistance);
+        foreach (var h in hits)
+        {
+            if (h.collider != null && h.collider.gameObject != gameObject)
+            {
+                direction *= -1;
+                spriteRenderer.flipX = direction.x > 0;
+                break;
+            }
+        }
         rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocity.y);
     }
 }
