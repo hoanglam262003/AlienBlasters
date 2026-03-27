e34fd891850f14d258d3738e4815f940da3919ae
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2026-03-27 16:26:57 +0700

    Apply laser burst for laser

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,6 +6,8 @@ public class Laser : MonoBehaviour
     private Vector2 direction = Vector2.right;
     [SerializeField]
     private float distance = 10f;
+    [SerializeField]
+    private SpriteRenderer laserBurst;
     private LineRenderer lineRenderer;
     private bool isOn;
 
@@ -17,18 +19,28 @@ public class Laser : MonoBehaviour
 
     private void Update()
     {
-        if (isOn == false) return;
-
+        if (isOn == false)
+        {
+            laserBurst.enabled = false;
+            return;
+        }
         var endPoint = (Vector2)transform.position + direction.normalized * distance;
         var hit = Physics2D.Raycast(transform.position, direction, distance);
         if (hit.collider) 
         {
             endPoint = hit.point;
+            laserBurst.transform.position = endPoint;
+            laserBurst.enabled = true;
+            laserBurst.transform.localScale = Vector3.one * (0.5f + Mathf.PingPong(Time.time, 1f));
             var takeLaserDamage = hit.collider.GetComponent<ITakeLaserDamage>();
             if (takeLaserDamage != null)
             {
                 takeLaserDamage.TakeLaserDamage();
             }
+        }
+        else
+        {
+            laserBurst.enabled = false;
         }
         lineRenderer.SetPosition(1, endPoint);
     }
