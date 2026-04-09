02b382c449b96c05ded3f7c0ad026e0eafbce7e8
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2026-04-09 09:15:13 +0700

    Ignore from top for enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,8 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private bool ignoreFromTop;
     protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
+        if (ignoreFromTop && Vector2.Dot(collision.contacts[0].normal, Vector2.down) > 0.5f)
+            return;
         var player = collision.collider.GetComponent<Player>();
         if (player != null)
         {
